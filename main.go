0609a27486dd7edb04aa0910924f4d1fb0794146
Mahdi Ashrafee <ashrafeem8@gmail.com>
2026-08-22 00:51:30 +0600

    refactor: introduce shared config struct for REPL commands

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
 func main() {
-	repl()
+	c := config{}
+	c.commands = make(map[string]cliCommands)
+	c.commands["exit"] = cliCommands{
+		name:        "exit",
+		description: "Closing the Pokedex... Goodbye!",
+		callback:    commandExit,
+	}
+	c.commands["help"] = cliCommands{
+		name:        "help",
+		description: "Welcome to the Pokedex!\nUsage:\nhelp: Displays a help message\nexit: Exit the Pokedex",
+		callback:    commandHelp,
+	}
+	repl(&c)
 }
--- repl.go
+++ repl.go
@@ -7,54 +7,45 @@ import (
 	"strings"
 )
 
+type cliCommands struct {
+	name        string
+	description string
+	callback    func(*config) error
+}
+type config struct {
+	commands map[string]cliCommands
+}
+
 func cleanInput(text string) []string {
 	words := strings.Fields(strings.ToLower(text))
 	return words
 }
 
-func commandExit() error {
+func commandExit(c *config) error {
 	fmt.Print("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
-func commandHelp() error {
+func commandHelp(c *config) error {
 	fmt.Println("Welcome to the Pokedex!\nUsage:\nhelp: Displays a help message\nexit: Exit the Pokedex")
 	return nil
 }
 
-func repl() {
+func repl(c *config) {
 	scanner := bufio.NewScanner(os.Stdin)
-	type cliCommands struct {
-		name        string
-		description string
-		callback    func() error
-	}
-
-	commands := map[string]cliCommands{
-		"exit": {
-			name:        "exit",
-			description: "Closing the Pokedex... Goodbye!",
-			callback:    commandExit,
-		},
-		"help": {
-			name:        "exit",
-			description: "Welcome to the Pokedex!\nUsage:\nhelp: Displays a help message\nexit: Exit the Pokedex",
-			callback:    commandHelp,
-		},
-	}
 
 	for {
 		fmt.Print("pokedex > ")
 		scanner.Scan()
 		commandName := strings.ToLower(strings.Fields(scanner.Text())[0])
-		command, exists := commands[commandName]
+		command, exists := c.commands[commandName]
 
 		if !exists {
 			fmt.Println("Unknown command")
 			continue
 		}
 
-		err := command.callback()
+		err := command.callback(c)
 		if err != nil {
 			fmt.Print("Unknown command")
 		}
